26edd9a5535a35986ba32ab957bf77bb00ff8253
AbrahamOliden <abrahamoliden@protonmail.com>
2024-05-14 22:17:59 -0600

    add click event listener to takePhotoButton

--- js/camera.js
+++ js/camera.js
@@ -1,6 +1,10 @@
 const openCameraButton = document.querySelector('[data-video-boton]');
 const video = document.querySelector('[data-video]');
 const cameraInput = document.querySelector('[data-camera]');
+const takePhotoButton = document.querySelector('[data-tomar-foto]');
+const message = document.querySelector('[data-mensaje]');
+const canvas = document.querySelector('[data-video-canvas]');
+let imgUrl = '';
 
 openCameraButton.addEventListener('click', async () => {
     const startVideo = await navigator.mediaDevices.getUserMedia({video: true, audio: false});
@@ -8,4 +12,11 @@ openCameraButton.addEventListener('click', async () => {
     openCameraButton.style.display = 'none';
     cameraInput.style.display = 'block';
     video.srcObject = startVideo;
+});
+
+takePhotoButton.addEventListener('click', () => {
+    canvas.getContext('2d').drawImage(video, 0, 0, canvas.width, canvas.height);
+    imgUrl = canvas.toDataURL('image/jpeg');
+    cameraInput.style.display = 'none';
+    message.style.display = 'block';
 });
