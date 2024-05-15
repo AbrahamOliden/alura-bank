dad2fe8c59caca1db4e7542f20cea75310628820
AbrahamOliden <abrahamoliden@protonmail.com>
2024-05-14 22:01:41 -0600

    add click event listener to openCameraButton

--- js/camera.js
+++ js/camera.js
@@ -1,3 +1,11 @@
 const openCameraButton = document.querySelector('[data-video-boton]');
 const video = document.querySelector('[data-video]');
 const cameraInput = document.querySelector('[data-camera]');
+
+openCameraButton.addEventListener('click', async () => {
+    const startVideo = await navigator.mediaDevices.getUserMedia({video: true, audio: false});
+
+    openCameraButton.style.display = 'none';
+    cameraInput.style.display = 'block';
+    video.srcObject = startVideo;
+});
